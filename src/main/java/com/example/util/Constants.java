6a49e2e96bc0e0156854686c2b65ab1cc52e9f68
mirkomil_a <mirkomilablayev777@gmail.com>
2024-12-13 16:16:17 +0500

    initial commit

--- src/main/java/com/example/util/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.util;
-
-public interface Constants {
-    String USER_ROLE = "USER";
-    String ADMIN_ROLE = "ADMIN";
-}
--- src/main/java/com/example/util/UserStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.util;
-
-public interface UserStatus {
-    String GUESS_USER = "GUESS_USER";
-    String AUTHORIZED_USER = "AUTHORIZED_USER";
-    String AUTHORIZED_CLIENT_USER = "AUTHORIZED_CLIENT_USER";
-}
